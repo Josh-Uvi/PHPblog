58c69fb1bdbd242bece5983a4d1b18d49dec87cb
Joshua Uvbiekpahor <40158394+Josh-Uvi@users.noreply.github.com>
2018-08-05 16:40:11 +0100

    first commit

---
+++ phpblog/config/config.php
@@ -0,0 +1,9 @@
+<?php  
+	define('ROOT_URL', 'http://localhost/sites/phpblog/');
+	//setting up db connection
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', 'Mypass07');
+	define('DB_NAME', 'phpblog');
+
+?>
